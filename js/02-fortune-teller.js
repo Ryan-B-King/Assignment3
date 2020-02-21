6db5b298f60c2220440b65d97634c679e35d14bd
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-02-20 20:40:49 -0800

    Completed JS2

---
+++ js/02-fortune-teller.js
@@ -0,0 +1,7 @@
+function tellFortune(children, partner, location, title) {
+    window.console.log("You will be a " + title + " in " + location + ", and married to " + partner + " with " + children + " kid(s).");
+}
+
+tellFortune("1", "Jim", "Texas", "Janitor" );
+tellFortune("2", "Amanda", "France", "Writer" );
+tellFortune("3", "Pablo", "Alaska", "Fisherman" );
