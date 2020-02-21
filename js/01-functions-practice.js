842d4b97026dc8187624e6f905fb7ba809a8c4cc
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2020-02-20 20:21:35 -0800

    Completed part 1

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,11 +1,74 @@
 //STEP 1
 function halfNumber(num) {
-    
+    "use strict";
+    let half;
+    half = num / 2;
+    return window.console.log("Half of " + num + " is " + half + ".");
 }
+
+// TEST FUNCTION CALLS
+halfNumber(5);
+halfNumber(20);
+
+// ****************************************************
+
 //STEP 2
+function squareNumber(num) {
+    "use strict";
+    let square;
+    square = num * num;
+    return window.console.log("The result of squaring the number " + num + " is " + square + ".");
+}
+
+// TEST FUNCTION CALLS
+squareNumber(3);
+squareNumber(10);
+
+// ****************************************************
 
 //STEP 3
 
+function percentOf(num1, num2) {
+    "use strict";
+    let percentage;
+    percentage =  parseFloat((num1 / num2) * 100);
+    return window.console.log(num1 + " is " + percentage + "% of " + num2 + ".");
+}
+
+// TEST FUNCTION CALLS
+percentOf(2, 4);
+percentOf(3, 12);
+
+// ****************************************************
+
 //STEP 4
 
+function findModulus(num1, num2) {
+    "use strict";
+    let modulus;
+    modulus = num1 % num2;
+    return window.console.log(modulus + " is the modulus of " + num2 + " and " + num1 + ".");
+}
+
+findModulus(10, 4);
+findModulus(5, 1);
+
+// ****************************************************
+
 //STEP 5
+
+function addTotal() {
+    let var1;
+    let var2;
+    let var3;
+    let total;
+
+    var1 = Number(window.prompt("Enter first number to add."));
+    var2 = Number(window.prompt("Enter second number to add."));
+    var3 = Number(window.prompt("Enter third number to add."));
+    total = var1 + var2 + var3;
+
+    return window.console.log("The total sum of: " + var1 + ", " + var2 + ", and " + var3 + " equals " + total + ".");
+}
+
+addTotal();
